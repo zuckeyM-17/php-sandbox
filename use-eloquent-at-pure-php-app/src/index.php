029ad505b1e46c579de8ea25a3e929d62a5443be
松村和輝 <matsumura@zuckeys-mbp.local>
2018-01-08 02:57:46 +0900

    eloquentのcollectionを利用するように変更

--- use-eloquent-at-pure-php-app/src/index.php
+++ use-eloquent-at-pure-php-app/src/index.php
@@ -5,11 +5,9 @@ require_once(SRC_DIR . '/vendor/autoload.php');
 require_once(SRC_DIR . '/database.php');
 require_once(SRC_DIR. '/Book.php');
 
-$books = Book::all()->toArray();
+$books = Book::all();
 
-if (!empty($books)) {
-    foreach($books as $book) {
-        echo $book['isbn'], "\t" , ($book['name']), "\t", $book['author'], "\t", $book['created'], "\t", $book['updated'], PHP_EOL;
-    }
+foreach($books as $book) {
+    echo $book->isbn, "\t" , $book->name, "\t", $book->author, "\t", $book->created, "\t", $book->updated, PHP_EOL;
 }
 
