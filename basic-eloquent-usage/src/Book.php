8b4fba1f9ec5de6784123bb6771a415dce236763
松村和輝 <k.matsumura0314@gmail.com>
2018-01-14 21:27:02 +0900

    require Global Scope

--- basic-eloquent-usage/src/Book.php
+++ basic-eloquent-usage/src/Book.php
@@ -1,5 +1,7 @@
 <?php
 require_once dirname(__FILE__).'/database.php';
+require_once dirname(__FILE__).'/BookScope.php';
+
 use Illuminate\Database\Eloquent\Model;
 class Book extends Model {}    protected static function boot()
     protected static function boot()
