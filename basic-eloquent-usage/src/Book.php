30a0ca125219060c84ba52744c3ecb606216c1b6
松村和輝 <k.matsumura0314@gmail.com>
2018-01-14 21:27:19 +0900

    implement fillable

--- basic-eloquent-usage/src/Book.php
+++ basic-eloquent-usage/src/Book.php
@@ -3,10 +3,15 @@ require_once dirname(__FILE__).'/database.php';
 require_once dirname(__FILE__).'/BookScope.php';
 
 use Illuminate\Database\Eloquent\Model;
-class Book extends Model {}    protected static function boot()
+class Book extends Model {
+    protected $fillable = [
+        'isbn',
+        'name',
+        'author',
+    ];
+
     protected static function boot()
     {
         parent::boot();
 
         static::addGlobalScope(new BookScope);
-    }
