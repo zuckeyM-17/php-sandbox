957f274c591a30c34ddefb84b18a8324b82c8456
松村和輝 <k.matsumura0314@gmail.com>
2018-01-14 21:26:35 +0900

    implement addGlobalScope

--- basic-eloquent-usage/src/Book.php
+++ basic-eloquent-usage/src/Book.php
@@ -1,4 +1,10 @@
 <?php
 require_once dirname(__FILE__).'/database.php';
 use Illuminate\Database\Eloquent\Model;
-class Book extends Model {}
+class Book extends Model {}    protected static function boot()
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new BookScope);
+    }
