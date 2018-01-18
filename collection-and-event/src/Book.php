5bcbaf0b9e96aee4e6f8dbf8cd12379051337d1b
松村和輝 <k.matsumura0314@gmail.com>
2018-01-19 04:19:37 +0900

    use softdelete

--- collection-and-event/src/Book.php
+++ collection-and-event/src/Book.php
@@ -2,7 +2,12 @@
 require_once dirname(__FILE__).'/database.php';
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Book extends Model {
+
+    use SoftDeletes;
+
     protected $fillable = [
         'isbn',
         'name',
