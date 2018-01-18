d94bbbf31d2fab547f4b73cef9c164b808d0517e
松村和輝 <k.matsumura0314@gmail.com>
2018-01-19 04:00:31 +0900

    remove scope

--- collection-and-event/src/Book.php
+++ collection-and-event/src/Book.php
@@ -1,6 +1,5 @@
 <?php
 require_once dirname(__FILE__).'/database.php';
-require_once dirname(__FILE__).'/BookScope.php';
 
 use Illuminate\Database\Eloquent\Model;
 class Book extends Model {
@@ -9,21 +8,4 @@ class Book extends Model {
         'name',
         'author',
     ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope(new BookScope);
-    }
-
-    public function scopeRecent($query)
-    {
-        return $query->where('id', '>', 3);
-    }
-
-    public function scopeOfId($query, $id)
-    {
-        return $query->where('id', $id);
-    }
 }
