b323c76effeac826d70af8221cee41acc579934d
松村和輝 <k.matsumura0314@gmail.com>
2018-01-26 14:34:42 +0900

    マイグレーションをimmutableに、かつ同じホテル、旅館を複数登録しレコード数をカサ増し

--- check-memory-usage/src/migration.php
+++ check-memory-usage/src/migration.php
@@ -3,6 +3,17 @@
 define('SRC_DIR', dirname(__FILE__));
 
 if (!file_exists(SRC_DIR.'/app.db')) {
+    prepareDb();
+} else {
+    echo '/app.db exists', PHP_EOL; flush();
+    echo 'delete /app.db ...'; flush();
+    unlink(SRC_DIR.'/app.db');
+    echo 'done', PHP_EOL; flush();
+    prepareDb();
+}
+
+
+function prepareDb() {
     try {
         echo 'create /app.db ... '; flush();
         if (($db = new PDO('sqlite:' . SRC_DIR . '/app.db')) && ($db->query('PRAGMA journal_mode=TRUNCATE;') !== false)) {
@@ -13,7 +24,7 @@ if (!file_exists(SRC_DIR.'/app.db')) {
 
             $db->query('CREATE TABLE IF NOT EXISTS `inns` ('.PHP_EOL.
                 '    `id`         INTEGER PRIMARY KEY AUTOINCREMENT,'.PHP_EOL.
-                '    `key`        TEXT UNIQUE NOT NULL,'.PHP_EOL.
+                '    `key`        TEXT NOT NULL,'.PHP_EOL.
                 '    `name`       TEXT NOT NULL,'.PHP_EOL.
                 '    `address`    TEXT NOT NULL,'.PHP_EOL.
                 '    `tel`        TEXT,' .PHP_EOL.
@@ -23,21 +34,23 @@ if (!file_exists(SRC_DIR.'/app.db')) {
                 '    `position_info_code` TEXT'.PHP_EOL.');');
             echo 'done',PHP_EOL; flush();
 
-            $file = fopen("./hotel_ichiran201712.csv", "r");
-            while($line = fgetcsv($file)){
-                $stmt = $db->prepare('INSERT INTO inns (`key`, `name`, `address`, `tel`, `url`, `longitude`, `latitude`, `position_info_code`) VALUES ' . PHP_EOL .
-                '("' . $line[0] . '", "' . $line[1] . '", "' . $line[2] . '", "' . $line[3] . '", "' . $line[4] . '", "' . $line[5] . '", "' . $line[6] . '", "' . $line[7] . '");');
-                $stmt->execute();
-            }
-            fclose($file);
+            for ($i = 0; $i < 40; $i++) {
+                $file = fopen("./hotel_ichiran201712.csv", "r");
+                while($line = fgetcsv($file)){
+                    $stmt = $db->prepare('INSERT INTO inns (`key`, `name`, `address`, `tel`, `url`, `longitude`, `latitude`, `position_info_code`) VALUES ' . PHP_EOL .
+                    '("' . $line[0] . '", "' . $line[1] . '", "' . $line[2] . '", "' . $line[3] . '", "' . $line[4] . '", "' . $line[5] . '", "' . $line[6] . '", "' . $line[7] . '");');
+                    $stmt->execute();
+                }
+                fclose($file);
 
-            $file = fopen("./ryokan_ichiran201712.csv", "r");
-            while($line = fgetcsv($file)){
-                $stmt = $db->prepare('INSERT INTO inns (`key`, `name`, `address`, `tel`, `url`, `longitude`, `latitude`, `position_info_code`) VALUES ' . PHP_EOL .
-                '("' . $line[0] . '", "' . $line[1] . '", "' . $line[2] . '", "' . $line[3] . '", "' . $line[4] . '", "' . $line[5] . '", "' . $line[6] . '", "' . $line[7] . '");');
-                $stmt->execute();
+                $file = fopen("./ryokan_ichiran201712.csv", "r");
+                while($line = fgetcsv($file)){
+                    $stmt = $db->prepare('INSERT INTO inns (`key`, `name`, `address`, `tel`, `url`, `longitude`, `latitude`, `position_info_code`) VALUES ' . PHP_EOL .
+                    '("' . $line[0] . '", "' . $line[1] . '", "' . $line[2] . '", "' . $line[3] . '", "' . $line[4] . '", "' . $line[5] . '", "' . $line[6] . '", "' . $line[7] . '");');
+                    $stmt->execute();
+                }
+                fclose($file);
             }
-            fclose($file);
 
             $db->commit();
         }
@@ -48,6 +61,4 @@ if (!file_exists(SRC_DIR.'/app.db')) {
     catch (Exception $e) {
         echo $e->getMessage();
     }
-} else {
-    echo '/app.db exists';
 }
